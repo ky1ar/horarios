3e4a490e25a395f7676793483b4ed5e5e1e6ee64
Kenny Muñoz <kenny@munoz.pe>
2024-03-28 23:06:05 -0500

    s

--- index.php
+++ index.php
@@ -150,8 +150,7 @@ require_once 'db.php';
                     <li>
                         <?php 
                         if ($row['stamp']) {
-                            echo $row['stamp'];
-                            $array = str_split(trim($row['stamp']), 4);
+                            $array = str_split(trim($row['stamp']), 5);
                             foreach ($array as $value):?>
                             <li><?php echo $value ?></li>
                             <?php endforeach;
