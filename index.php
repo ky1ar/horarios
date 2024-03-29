2a0c948f8eacf68085ca9ebaea195accdb9c6314
Kenny Muñoz <kenny@munoz.pe>
2024-03-28 23:21:33 -0500

    s

--- index.php
+++ index.php
@@ -147,7 +147,7 @@ require_once 'db.php';
                         <span>Semana</span>
                             <div class='hrr-day'>";
                 } 
-                if ($day_week < 5 || $row['stamp']) {
+                if ($row['stamp']) {
                     echo
                     "<ul>
                         <li class='day-nam'>$day</li>";
