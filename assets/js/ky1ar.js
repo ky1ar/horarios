7519bdef1c7673c76ccd214c96d5a43d8913b12c
alex <alex@gmail.com>
2024-05-15 11:40:01 -0500

    edt

--- assets/js/ky1ar.js
+++ assets/js/ky1ar.js
@@ -55,24 +55,28 @@ $(document).ready(function () {
 });
 
 function getUserSchedule(userId) {
-  $.ajax({
-    url: "../routes/del/get_user_schedule.php",
-    method: "POST",
-    data: { userId: userId },
-    dataType: "json",
-    success: function (response) {
+    $.ajax({
+      url: "../routes/del/get_user_schedule.php",
+      method: "POST",
+      data: { userId: userId },
+      dataType: "json",
+      success: function (response) {
         if (response.success) {
           // Limpiar contenido existente
           $(".hrr-day").empty();
-          
+  
           // Recorrer los datos del horario del usuario
           response.schedule.forEach(function (entry) {
             var $dayList = $("<ul></ul>").appendTo(".hrr-day");
             var dayName = entry.day_name; // Supongamos que hay un campo que indica el nombre del día
-            
-            // Agregar el nombre del día como primer elemento de la lista
-            $("<li class='day-nam'>" + dayName + "</li>").appendTo($dayList);
-            
+            var dateNumber = entry.date_number; // Supongamos que hay un campo que indica el número de la fecha
+  
+            // Formatear el nombre del día junto con el número de la fecha
+            var formattedDayName = getFormattedDayName(dayName, dateNumber);
+  
+            // Agregar el nombre del día junto con el número de la fecha como primer elemento de la lista
+            $("<li class='day-nam'>" + formattedDayName + "</li>").appendTo($dayList);
+  
             // Dividir el sello de tiempo en intervalos y agregarlos como elementos de la lista
             var stamps = entry.stamp.split(",");
             stamps.forEach(function (stamp) {
@@ -83,9 +87,30 @@ function getUserSchedule(userId) {
           console.error(response.message);
         }
       },
-      
-    error: function (xhr, status, error) {
-      console.error("Error en la solicitud AJAX:", error);
-    },
-  });
-}
+  
+      error: function (xhr, status, error) {
+        console.error("Error en la solicitud AJAX:", error);
+      },
+    });
+  }
+  
+  // Función para formatear el nombre del día y el número de la fecha
+  function getFormattedDayName(dayName, dateNumber) {
+    // Definir un objeto para mapear los nombres de los días abreviados en español
+    var dayNames = {
+      "lun": "lunes",
+      "mar": "martes",
+      "mie": "miércoles",
+      "jue": "jueves",
+      "vie": "viernes",
+      "sab": "sábado",
+      "dom": "domingo"
+    };
+  
+    // Obtener el nombre completo del día basado en el nombre abreviado proporcionado
+    var fullDayName = dayNames[dayName.toLowerCase()];
+  
+    // Devolver el nombre completo del día junto con el número de la fecha
+    return fullDayName + " " + dateNumber;
+  }
+  
