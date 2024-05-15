edf204652ef648df07b149f3bf321c834e3ce476
alex <alex@gmail.com>
2024-05-15 10:46:29 -0500

    edt

--- assets/js/ky1ar.js
+++ assets/js/ky1ar.js
@@ -1,23 +1,3 @@
-// Definir la función getUserSchedule como una función global
-function getUserSchedule(userId) {
-    $.ajax({
-        url: '../routes/del/get_user_schedule.php', 
-        method: 'POST',
-        data: { userId: userId },
-        dataType: 'json',
-        success: function(response) {
-            if (response.success) {
-                console.log(response.schedule);
-            } else {
-                console.error(response.message);
-            }
-        },
-        error: function(xhr, status, error) {
-            console.error('Error en la solicitud AJAX:', error);
-        }
-    });
-}
-
 $(document).ready(function() {
     const nextUser = $('#nextUser');
     const previousUser = $('#previousUser');
@@ -49,18 +29,13 @@ $(document).ready(function() {
         userImage.attr('src', imagePath + newUser.data('slug') + '.png');
         userName.text(newUser.data('name'));
         userCategory.text(newUser.data('category'));
-        
-        // Llamar a la función getUserSchedule cuando se actualiza el usuario
+
         getUserSchedule(newUser.data('id'));  
     }
 
-    // Evento de clic para el botón "Siguiente Usuario"
     nextUser.on('click', function() { updateUser(1); });  
-    
-    // Evento de clic para el botón "Usuario Anterior"
     previousUser.on('click', function() { updateUser(-1); });
 
-    // Evento de clic para los elementos de la lista de usuarios
     userList.find('li').on('click', function() { 
         let $this = $(this);
         userList.find('li').removeClass('active');
@@ -71,7 +46,25 @@ $(document).ready(function() {
         userName.text($this.data('name'));
         userCategory.text($this.data('category'));
 
-        // Llamar a la función getUserSchedule cuando se hace clic en un usuario
         getUserSchedule($this.data('id'));  
     });
 });
+
+function getUserSchedule(userId) {
+    $.ajax({
+        url: '../routes/del/get_user_schedule.php', 
+        method: 'POST',
+        data: { userId: userId },
+        dataType: 'json',
+        success: function(response) {
+            if (response.success) {
+                console.log(response.schedule);
+            } else {
+                console.error(response.message);
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error('Error en la solicitud AJAX:', error);
+        }
+    });
+}
