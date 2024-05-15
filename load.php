b632bc36735ac9fc0b2586a11fe6f184e7d39d93
alex <alex@gmail.com>
2024-05-15 16:53:25 -0500

    primera version

--- load.php
+++ load.php
@@ -109,60 +109,7 @@ require_once 'db.php';
             </li>
         </ul>
         <ul class="ky1-hrr">
-            <li class="hrr-box">
-                <span>Semana 1</span>
-                <div class="hrr-day">
-                    <!--<ul>
-                        <li class="day-nam"></li>
-                        <li><img src="assets/img/a.svg" width="20" height="20" alt=""></li>
-                        <li><img src="assets/img/b.svg" width="20" height="20" alt=""></li>
-                        <li><img src="assets/img/c.svg" width="20" height="20" alt=""></li>
-                        <li><img src="assets/img/d.svg" width="20" height="20" alt=""></li>
-                    </ul>-->
-                    <ul>
-                        <li class="day-nam">lun 1</li>
-                        <li class="day-trd">09:11</li>
-                        <li>13:10</li>
-                        <li>14:08</li>
-                        <li>18:05</li>
-                    </ul>
-                    <ul>
-                        <li class="day-nam">mar 2</li>
-                        <li>09:00</li>
-                        <li class="day-rgs">13:10</li>
-                        <li>14:08</li>
-                        <li>18:05</li>
-                    </ul>
-                    <ul>
-                        <li class="day-nam">mie 3</li>
-                        <li>09:09</li>
-                        <li>13:10</li>
-                        <li>14:08</li>
-                        <li>18:05</li>
-                    </ul>
-                    <ul>
-                        <li class="day-nam">jue 4</li>
-                        <li>09:09</li>
-                        <li>13:10</li>
-                        <li>14:08</li>
-                        <li>18:05</li>
-                    </ul>
-                    <ul>
-                        <li class="day-nam">vie 5</li>
-                        <li>09:09</li>
-                        <li>13:10</li>
-                        <li>14:08</li>
-                        <li>18:05</li>
-                    </ul>
-                    <ul>
-                        <li class="day-nam">sab 6</li>
-                        <li>09:09</li>
-                        <li>13:10</li>
-                        <li>14:08</li>
-                        <li>18:05</li>
-                    </ul>
-                </div>
-            </li>
+            
         </ul>
 
         <?php
