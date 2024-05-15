93b21e942dc4b1c7e4898f7d8b059373517af648
alex <alex@gmail.com>
2024-05-15 09:45:22 -0500

    edt

--- index1.php
+++ index1.php
@@ -8,7 +8,7 @@ require_once 'db.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="assets/img/fav.png">
+    <!-- <link rel="icon" href="assets/img/fav.png"> -->
     <title>Krear 3D | Horarios</title>
     <?php require_once 'header.php'; ?>
 </head>
