d428edcbd088998b450877c4b2d594f41f6bd18b
alex <alex@gmail.com>
2024-05-15 14:27:46 -0500

    edt

--- assets/js/ky1ar.js
+++ assets/js/ky1ar.js
@@ -63,31 +63,40 @@ function getUserSchedule(userId) {
       success: function (response) {
         if (response.success) {
           $(".ky1-hrr").empty();
-
-          var daysCounter = 0; 
-          var $currentHrrBox; 
-          response.schedule.forEach(function (entry, index) {
-            if (index % 6 === 0) {
-              $currentHrrBox = $("<li class='hrr-box'></li>").appendTo(".ky1-hrr");
-              $("<span>Semana " + (Math.floor(index / 6) + 1) + "</span>").appendTo($currentHrrBox);
-              $("<div class='hrr-day'></div>").appendTo($currentHrrBox);
-            }
+          var daysOfWeek = ['lun', 'mar', 'mie', 'jue', 'vie', 'sab']; // Array de nombres de los días de la semana
   
-            var $hrrDay = $currentHrrBox.find('.hrr-day');
-            var $dayList = $("<ul></ul>").appendTo($hrrDay);
-            var dayName = entry.day_name_espanol.substring(0, 3);
-            var dayNumber = entry.day_number; 
-
-            $("<li class='day-nam'>" + dayName + " " + dayNumber + "</li>").appendTo($dayList);
-            var stamps = entry.stamp.split(",");
-            stamps.forEach(function (stamp) {
-              for (var i = 0; i < stamp.length; i += 5) {
-                $("<li>" + stamp.slice(i, i + 5) + "</li>").appendTo($dayList);
+          // Iterar sobre los datos para construir las semanas
+          var weeks = [];
+          response.schedule.forEach(function (entry, index) {
+            var dayIndex = (entry.day_number - 1) % 7; // Obtener el índice del día en la semana
+            var weekIndex = Math.floor(index / 6); // Obtener el índice de la semana
+            if (!weeks[weekIndex]) weeks[weekIndex] = []; // Inicializar la semana si es la primera vez que se encuentra
+            weeks[weekIndex][dayIndex] = entry; // Agregar el dato al día correspondiente
+          });
+  
+          // Iterar sobre las semanas para construir la estructura HTML
+          weeks.forEach(function (week, weekIndex) {
+            var $currentHrrBox = $("<li class='hrr-box'></li>").appendTo(".ky1-hrr");
+            $("<span>Semana " + (weekIndex + 1) + "</span>").appendTo($currentHrrBox);
+            var $hrrDay = $("<div class='hrr-day'></div>").appendTo($currentHrrBox);
+  
+            // Iterar sobre los días de la semana actual
+            daysOfWeek.forEach(function (dayName, dayIndex) {
+              var dayData = week[dayIndex] || { day_name_espanol: '', day_number: '' }; // Obtener los datos del día o un objeto vacío si no hay datos
+              var $dayList = $("<ul></ul>").appendTo($hrrDay);
+              $("<li class='day-nam'>" + dayName + " " + dayData.day_number + "</li>").appendTo($dayList);
+  
+              // Agregar las horas correspondientes si hay datos para este día
+              if (dayData.stamp) {
+                var stamps = dayData.stamp.split(",");
+                stamps.forEach(function (stamp) {
+                  for (var i = 0; i < stamp.length; i += 5) {
+                    $("<li>" + stamp.slice(i, i + 5) + "</li>").appendTo($dayList);
+                  }
+                });
               }
             });
-            daysCounter++;
           });
-          console.log(response.schedule);
         } else {
           console.error(response.message);
         }
