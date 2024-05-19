4e19bafddd438038601067136178ab283ebef0aa
adriannc21 <alexandernajarro17@gmail.com>
2024-05-18 22:51:19 -0500

    first v

--- routes/del/get_time_difference.php
+++ routes/del/get_time_difference.php
@@ -202,10 +202,10 @@ FROM
             END AS new_column
         FROM 
             Calendar c
-        LEFT JOIN Schedule s ON c.id_date = s.id_calendar AND s.id_user = 12
+        LEFT JOIN Schedule s ON c.id_date = s.id_calendar AND s.id_user = ?
         LEFT JOIN Users u ON s.id_user = u.id_user
         WHERE
-            c.calendar_date = '2024-05-02'
+            c.calendar_date = ?
     ) AS t;";
 
     $stmt = $conn->prepare($query);
