0656cdfd2fdd937117510f02572bf252099bbc53
adriannc21 <alexandernajarro17@gmail.com>
2024-05-19 19:35:43 -0500

    edt first version

--- assets/js/ky1ar.js
+++ assets/js/ky1ar.js
@@ -26,8 +26,8 @@ $(document).ready(function () {
     "Noviembre",
     "Diciembre",
   ];
-  let currentMonth = new Date().getMonth() + 1; // Mes actual (1 a 12)
-  let currentYear = new Date().getFullYear(); // Año actual
+  let currentMonth = new Date().getMonth() + 1; 
+  let currentYear = new Date().getFullYear(); 
 
   function updateMonthDisplay() {
     $(".ky1-dte span").text(`${monthNames[currentMonth - 1]}, ${currentYear}`);
@@ -57,7 +57,7 @@ $(document).ready(function () {
     newUser.addClass("active");
     updateUserDisplay();
 
-    getUserSchedule(newUser.data("id"), currentMonth, currentYear); // Pasa mes y año actual
+    getUserSchedule(newUser.data("id"), currentMonth, currentYear);
   }
 
   nextUser.on("click", function () {
@@ -71,22 +71,21 @@ $(document).ready(function () {
     currentMonth = (currentMonth % 12) + 1;
     if (currentMonth === 1) currentYear++;
     updateMonthDisplay();
-    getUserSchedule(selectedUser.attr("data-id"), currentMonth, currentYear); // Pasa mes y año actual
+    getUserSchedule(selectedUser.attr("data-id"), currentMonth, currentYear); 
   });
 
   previousMonth.on("click", function () {
     currentMonth = currentMonth === 1 ? 12 : currentMonth - 1;
     if (currentMonth === 12) currentYear--;
     updateMonthDisplay();
-    getUserSchedule(selectedUser.attr("data-id"), currentMonth, currentYear); // Pasa mes y año actual
+    getUserSchedule(selectedUser.attr("data-id"), currentMonth, currentYear); 
   });
 
   userList.find("li").on("click", function () {
     userList.find("li").removeClass("active");
     $(this).addClass("active");
     updateUserDisplay();
-
-    getUserSchedule($(this).data("id"), currentMonth, currentYear); // Pasa mes y año actual
+    getUserSchedule($(this).data("id"), currentMonth, currentYear); 
   });
   
   function getUserSchedule(userId, month, year) {
@@ -108,10 +107,8 @@ $(document).ready(function () {
             var dayName = entry.day_of_week_es;
             var dayNumber = entry.day_number;
             var hPoints = entry.time_difference;
-
-            // Omitir los domingos
             if (dayName.toLowerCase() === "domingo") {
-              return; // Salta este día y continúa con el siguiente
+              return; 
             }
 
             if (dayName.toLowerCase() === "lunes" || index === 0) {
@@ -150,7 +147,7 @@ $(document).ready(function () {
                 }
               });
             } else {
-              // Si no hay estampas, muestra un elemento vacío
+  
               $("<li></li>").appendTo($dayList);
             }
 
@@ -162,7 +159,6 @@ $(document).ready(function () {
                 "</li>"
             );
 
-            // Condiciones para cambiar el color del box-shadow
             if (hPoints === "DF") {
               $calcLi.css("box-shadow", "inset 0 -4rem 0 0 #F0DD38");
             } else if (hPoints.startsWith("+")) {
@@ -176,7 +172,7 @@ $(document).ready(function () {
             daysCounter++;
           });
 
-          console.log(response.schedule); // Verifica los datos recibidos
+          console.log(response.schedule); 
         } else {
           console.error(response.message);
         }
@@ -186,213 +182,10 @@ $(document).ready(function () {
       },
     });
   }
-
-  // Inicializa el mes y el usuario al cargar la página v2
   updateMonthDisplay();
   if (userList.find(".active").length === 0) {
     userList.find("li").first().addClass("active");
   }
   updateUserDisplay();
-  getUserSchedule(selectedUser.attr("data-id"), currentMonth, currentYear); // Cargar horario del usuario activo al inicio
+  getUserSchedule(selectedUser.attr("data-id"), currentMonth, currentYear); 
 });
-
-// $(document).ready(function () {
-//   const nextUser = $("#nextUser");
-//   const previousUser = $("#previousUser");
-//   const userList = $("#userList");
-//   const nextMonth = $("#nextMonth");
-//   const previousMonth = $("#previousMonth");
-
-//   const selectedUser = $("#selectedUser");
-//   const userName = $("#userName");
-//   const userCategory = $("#userCategory");
-
-//   const userImage = $("#userImage");
-//   const imagePath = "assets/img/profiles/";
-
-//   const monthNames = [
-//     "Enero",
-//     "Febrero",
-//     "Marzo",
-//     "Abril",
-//     "Mayo",
-//     "Junio",
-//     "Julio",
-//     "Agosto",
-//     "Septiembre",
-//     "Octubre",
-//     "Noviembre",
-//     "Diciembre",
-//   ];
-//   let currentMonth = new Date().getMonth() + 1; // Mes actual (1 a 12)
-//   let currentYear = new Date().getFullYear(); // Año actual
-
-//   function updateMonthDisplay() {
-//     $(".ky1-dte span").text(`${monthNames[currentMonth - 1]}, ${currentYear}`);
-//   }
-
-//   function updateUserDisplay() {
-//     const activeUser = userList.find(".active");
-//     selectedUser.attr("data-id", activeUser.data("id"));
-//     userImage.attr("src", imagePath + activeUser.data("slug") + ".png");
-//     userName.text(activeUser.data("name"));
-//     userCategory.text(activeUser.data("category"));
-//   }
-
-//   function updateUser(offset) {
-//     let current = userList.find(".active").index();
-//     let total = userList.find("li").length - 1;
-//     userList.find("li").removeClass("active");
-
-//     current = current + offset;
-//     if (offset == 1) {
-//       if (current > total) current = 0;
-//     } else if (offset == -1) {
-//       if (current < 0) current = total;
-//     }
-
-//     let newUser = userList.find("li").eq(current);
-//     newUser.addClass("active");
-//     updateUserDisplay();
-
-//     getUserSchedule(newUser.data("id"), currentMonth, currentYear); // Pasa mes y año actual
-//   }
-
-//   nextUser.on("click", function () {
-//     updateUser(1);
-//   });
-//   previousUser.on("click", function () {
-//     updateUser(-1);
-//   });
-
-//   nextMonth.on("click", function () {
-//     currentMonth = (currentMonth % 12) + 1;
-//     if (currentMonth === 1) currentYear++;
-//     updateMonthDisplay();
-//     getUserSchedule(selectedUser.attr("data-id"), currentMonth, currentYear); // Pasa mes y año actual
-//   });
-
-//   previousMonth.on("click", function () {
-//     currentMonth = currentMonth === 1 ? 12 : currentMonth - 1;
-//     if (currentMonth === 12) currentYear--;
-//     updateMonthDisplay();
-//     getUserSchedule(selectedUser.attr("data-id"), currentMonth, currentYear); // Pasa mes y año actual
-//   });
-
-//   userList.find("li").on("click", function () {
-//     userList.find("li").removeClass("active");
-//     $(this).addClass("active");
-//     updateUserDisplay();
-
-//     getUserSchedule($(this).data("id"), currentMonth, currentYear); // Pasa mes y año actual
-//   });
-
-//   function getUserSchedule(userId, month, year) {
-//     console.log(
-//       `Fetching schedule for userId: ${userId}, month: ${month}, year: ${year}`
-//     ); // Depuración
-//     $.ajax({
-//       url: "../routes/del/get_user_schedule.php",
-//       method: "POST",
-//       data: { userId: userId, month: month, year: year },
-//       dataType: "json",
-//       success: function (response) {
-//         if (response.success) {
-//           $(".ky1-hrr").empty();
-//           var daysCounter = 0;
-//           var $currentHrrBox;
-//           var currentWeek = 1;
-//           response.schedule.forEach(function (entry, index) {
-//             var dayName = entry.day_name_espanol;
-//             var dayNumber = entry.day_number;
-
-//             // Omitir los domingos
-//             if (dayName.toLowerCase() === "domingo") {
-//               return; // Salta este día y continúa con el siguiente
-//             }
-
-//             if (dayName.toLowerCase() === "lunes" || index === 0) {
-//               $currentHrrBox = $("<li class='hrr-box'></li>").appendTo(
-//                 ".ky1-hrr"
-//               );
-//               $("<span>Semana " + currentWeek + "</span>").appendTo(
-//                 $currentHrrBox
-//               );
-//               $("<div class='hrr-day'></div>").appendTo($currentHrrBox);
-//               currentWeek++; // Aumenta el contador de semana
-//             }
-
-//             var $hrrDay = $currentHrrBox.find(".hrr-day");
-//             var $dayList = $("<ul></ul>").appendTo($hrrDay);
-
-//             $(
-//               "<li class='day-nam'>" +
-//                 dayName.substring(0, 3) +
-//                 " " +
-//                 dayNumber +
-//                 "</li>"
-//             ).appendTo($dayList);
-
-//             if (entry.holiday == 1) {
-//               // Si es un feriado, muestra "FERIADO" en una sola línea
-//               $("<li class='test'>FERIADO</li>").appendTo($dayList);
-//             } else if (entry.stamp) {
-//               // Verifica si hay datos de estampas
-//               var stamps = entry.stamp.split(",");
-//               stamps.forEach(function (stamp) {
-//                 for (var i = 0; i < stamp.length; i += 5) {
-//                   $("<li>" + stamp.slice(i, i + 5) + "</li>").appendTo(
-//                     $dayList
-//                   );
-//                 }
-//               });
-//             } else {
-//               // Si no hay estampas, muestra un elemento vacío
-//               $("<li></li>").appendTo($dayList);
-//             }
-//             // console.log(
-//             //   `Solicitando diferencia de tiempo para userId: ${userId}, calendarDate: ${entry.calendar_date}`
-//             // );
-//             var formData = new FormData();
-//             formData.append("userId2", userId);
-//             formData.append("calendarDate", entry.calendar_date);
-//             $.ajax({
-//               url: "../routes/del/get_time_difference.php",
-//               method: "POST",
-//               data: formData,
-//               cache: false,
-//               contentType: false,
-//               processData: false,
-//               dataType: "json",
-//               success: function (response) {
-//                 console.log(response);
-//                 $(
-//                   "<li class='calc' data-date='" + entry.calendar_date + "'>"+ response.time_difference +"</li>"
-//                 ).appendTo($dayList);
-//               },
-//               error: function (xhr, status, error) {
-//                 console.error("Error en la solicitud AJAX:", error);
-//               },
-//             });
-//             // Añadir el elemento calc con los datos necesarios
-
-//             daysCounter++;
-//           });
-//         } else {
-//           console.error(response.message);
-//         }
-//       },
-//       error: function (xhr, status, error) {
-//         console.error("Error en la solicitud AJAX:", error);
-//       },
-//     });
-//   }
-
-//   // Inicializa el mes y el usuario al cargar la página
-//   updateMonthDisplay();
-//   if (userList.find(".active").length === 0) {
-//     userList.find("li").first().addClass("active");
-//   }
-//   updateUserDisplay();
-//   getUserSchedule(selectedUser.attr("data-id"), currentMonth, currentYear); // Cargar horario del usuario activo al inicio
-// });
