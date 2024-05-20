b7bf3691d78d24fc4d396c07fcb3a60a71bfc513
adriannc21 <alexandernajarro17@gmail.com>
2024-05-19 20:47:17 -0500

    edt first version

--- assets/js/ky1ar.js
+++ assets/js/ky1ar.js
@@ -57,7 +57,7 @@ $(document).ready(function () {
     newUser.addClass("active");
     updateUserDisplay();
     getUserSchedule(newUser.data("id"), currentMonth, currentYear);
-    getUserData(newUser.data("id"), currentMonth, currentYear)
+    getUserData(newUser.data("id"), currentMonth, currentYear);
   }
 
   nextUser.on("click", function () {
@@ -196,11 +196,18 @@ $(document).ready(function () {
     $("#totalUnjustifiedAbsences").text(totalUnjustifiedAbsences);
   }
   function getUserData(userId, month, year) {
-    console.log(`Data: ${userId}, month: ${month}, year: ${year}`); 
+    console.log(`Data: ${userId}, month: ${month}, year: ${year}`);
+    var formData = new FormData();
+    formData.append("userId", userId);
+    formData.append("month", month);
+    formData.append("year", year);
     $.ajax({
-      url: "../routes/dels/get_info_user.php",
+      url: "../routes/del/get_info_user.php",
       method: "POST",
-      data: { userId: userId, month: month, year: year },
+      data: formData,
+      cache: false,
+      contentType: false,
+      processData: false,
       dataType: "json",
       success: function (response) {
         console.log("Datos recibidos del servidor:", response);
@@ -221,7 +228,7 @@ $(document).ready(function () {
       },
     });
   }
-  
+
   updateMonthDisplay();
   if (userList.find(".active").length === 0) {
     userList.find("li").first().addClass("active");
