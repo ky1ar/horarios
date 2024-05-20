1ba2148831cfaa1ac484d5a29186b9da74f19eb5
adriannc21 <alexandernajarro17@gmail.com>
2024-05-19 22:44:43 -0500

    2v

--- routes/del/get_info_user.php
+++ routes/del/get_info_user.php
@@ -7,19 +7,11 @@ if (isset($_POST['userId']) && isset($_POST['month']) && isset($_POST['year'])) 
     $year = $_POST['year'];
     $currentDate = date('Y-m-d');
 
-    // Preparar la consulta SET SQL
-    $sql = "SET @year = ?;
-            SET @month = ?;
-            SET @id_user = ?;
-            SET @current_date = CURDATE();";
-
-    // Preparar y ejecutar la consulta SET
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("iii", $year, $month, $userId);
-    $stmt->execute();
-
-    // Preparar la consulta SELECT SQL
     $sql = "SELECT
+        u.id_user,
+        u.id_profile,
+        (
+            SELECT
                 SUM(
                     CASE
                         WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
@@ -32,16 +24,14 @@ if (isset($_POST['userId']) && isset($_POST['month']) && isset($_POST['year'])) 
                         WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 THEN 8
                         ELSE 0
                     END
-                ) INTO @total_hours
+                )
             FROM
                 Calendar c
-            JOIN
-                Users u ON u.id_user = @id_user
             WHERE
-                c.calendar_date BETWEEN DATE(CONCAT(@year, '-', @month, '-01')) AND LAST_DAY(DATE(CONCAT(@year, '-', @month, '-01')))
-                AND c.holiday = 0;
-
-            -- Calcular la sumatoria de missing points antes de la fecha actual
+                c.calendar_date BETWEEN DATE(CONCAT(?, '-', ?, '-01')) AND LAST_DAY(DATE(CONCAT(?, '-', ?, '-01')))
+                AND c.holiday = 0
+        ) AS total_hours_required,
+        (
             SELECT
                 SUM(
                     ROUND(
@@ -56,37 +46,33 @@ if (isset($_POST['userId']) && isset($_POST['month']) && isset($_POST['year'])) 
                             WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
                             ELSE 0
                         END, 0)
-                ) INTO @total_missing_points
+                )
             FROM
                 Calendar c
-            JOIN
-                Users u ON u.id_user = @id_user
             LEFT JOIN
-                Schedule s ON c.id_date = s.id_calendar AND s.id_user = @id_user
+                Schedule s ON c.id_date = s.id_calendar AND s.id_user = ?
             WHERE
-                c.calendar_date BETWEEN DATE(CONCAT(@year, '-', @month, '-01')) AND LAST_DAY(DATE(CONCAT(@year, '-', @month, '-01')))
-                AND c.calendar_date < @current_date
-                AND c.holiday = 0;
-
-            -- Calcular la sumatoria de llegadas tarde (is_late) para el mes
+                c.calendar_date BETWEEN DATE(CONCAT(?, '-', ?, '-01')) AND LAST_DAY(DATE(CONCAT(?, '-', ?, '-01')))
+                AND c.calendar_date < ?
+                AND c.holiday = 0
+        ) AS total_sin_registro,
+        (
             SELECT
                 SUM(
                     CASE
                         WHEN LEFT(s.stamp, 5) > '09:00' THEN 1
                         ELSE 0
                     END
-                ) INTO @total_late_points
+                )
             FROM
                 Calendar c
-            JOIN
-                Users u ON u.id_user = @id_user
             LEFT JOIN
-                Schedule s ON c.id_date = s.id_calendar AND s.id_user = @id_user
+                Schedule s ON c.id_date = s.id_calendar AND s.id_user = ?
             WHERE
-                c.calendar_date BETWEEN DATE(CONCAT(@year, '-', @month, '-01')) AND LAST_DAY(DATE(CONCAT(@year, '-', @month, '-01')))
-                AND c.holiday = 0;
-
-            -- Calcular la sumatoria de ausencias injustificadas hasta la fecha actual
+                c.calendar_date BETWEEN DATE(CONCAT(?, '-', ?, '-01')) AND LAST_DAY(DATE(CONCAT(?, '-', ?, '-01')))
+                AND c.holiday = 0
+        ) AS total_tardanzas,
+        (
             SELECT
                 SUM(
                     CASE
@@ -103,55 +89,35 @@ if (isset($_POST['userId']) && isset($_POST['month']) && isset($_POST['year'])) 
                             END) > 0
                             AND (s.stamp IS NULL OR LENGTH(s.stamp) = 0)
                             AND (c.just IS NULL OR c.just = '')
-                            AND c.calendar_date < @current_date THEN 1
+                            AND c.calendar_date < ? THEN 1
                         ELSE 0
                     END
-                ) INTO @total_unjustified_absences
+                )
             FROM
                 Calendar c
-            JOIN
-                Users u ON u.id_user = @id_user
             LEFT JOIN
-                Schedule s ON c.id_date = s.id_calendar AND s.id_user = @id_user
+                Schedule s ON c.id_date = s.id_calendar AND s.id_user = ?
             WHERE
-                c.calendar_date BETWEEN DATE(CONCAT(@year, '-', @month, '-01')) AND LAST_DAY(DATE(CONCAT(@year, '-', @month, '-01')))
-                AND c.holiday = 0;
+                c.calendar_date BETWEEN DATE(CONCAT(?, '-', ?, '-01')) AND LAST_DAY(DATE(CONCAT(?, '-', ?, '-01')))
+                AND c.holiday = 0
+        ) AS total_faltas_injustificadas
+    FROM
+        Users u
+    WHERE
+        u.id_user = ?
+    ";
 
-            -- Consulta final para obtener las columnas requeridas
-            SELECT
-                @id_user AS id_user,
-                u.id_profile,
-                @total_hours AS total_hours_required,
-                @total_missing_points AS total_sin_registro,
-                @total_late_points AS total_tardanzas,
-                @total_unjustified_absences AS total_faltas_injustificadas
-            FROM
-                Users u
-            WHERE
-                u.id_user = @id_user;";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("isssssiissssiissssiiss", $year, $month, $year, $month, $userId, $year, $month, $year, $month, $currentDate, $userId, $year, $month, $year, $month, $currentDate, $userId, $userId);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-    // Ejecutar la consulta con multi_query
-    if ($conn->multi_query($sql)) {
-        // Inicializar el array de datos finales
-        $finalData = array();
-
-        // Obtener los resultados y agregarlos al array finalData
-        do {
-            if ($result = $conn->store_result()) {
-                while ($row = $result->fetch_assoc()) {
-                    $finalData[] = $row;
-                }
-                $result->free();
-            }
-        } while ($conn->next_result());
-
-        // Imprimir los resultados en formato JSON
-        echo json_encode(array('success' => true, 'data' => $finalData));
-    } else {
-        // Si hay un error en la consulta, imprimir un mensaje de error
-        echo json_encode(array('success' => false, 'message' => 'Error al ejecutar la consulta.'));
+    $finalData = array();
+    while ($row = $result->fetch_assoc()) {
+        $finalData[] = $row;
     }
+    echo json_encode(array('success' => true, 'data' => $finalData));
 } else {
-    // Si no se recibieron los parámetros necesarios, imprimir un mensaje de error
     echo json_encode(array('success' => false, 'message' => 'No se recibieron los parámetros necesarios.'));
 }
+?>
