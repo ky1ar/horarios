a9935ce3db65d3591e2b495c21ef61d61a68f78c
adriannc21 <alexandernajarro17@gmail.com>
2024-05-26 21:20:29 -0500

    first v

--- assets/js/ky1ar.js
+++ assets/js/ky1ar.js
@@ -329,18 +329,18 @@ $(document).ready(function () {
         var totalMinutosActual = sumaHoras * 60 + sumaMinutos;
         var porcentaje = (totalMinutosActual / totalMinutosAcumulado) * 100;
         console.log(totalMinutosActual)
-        
-        function sumarRestarHoras(horaInicial, minutosASumarORestar, restar = false) {
-          const [horas, minutos] = horaInicial.split(':').map(Number);
-          const [horas2, minutos2] = minutosASumarORestar.split(':').map(Number);
-          const totalMinutos = horas * 60 + minutos;
-          const totalminutos2 = horas2 * 60 + minutos2;
-          const signo = restar ? -1 : 1;
-          const nuevoTotalMinutos = totalMinutos + signo * totalminutos2;
-      
-          const nuevaHora = `${Math.floor(nuevoTotalMinutos / 60)}:${(nuevoTotalMinutos % 60).toString().padStart(2, '0')}`;
-          return nuevaHora;
-      }
+
+        function sumarRestarHoras(totalMinutosActual, resultado, restar = false) {
+        const [horas, minutos] = totalMinutosActual.split(':').map(Number);
+        const [horas2, minutos2] = resultado.split(':').map(Number);
+        const totalMinutos = horas * 60 + minutos;
+        const totalminutos2 = horas2 * 60 + minutos2;
+        const signo = restar ? -1 : 1;
+        const nuevoTotalMinutos = totalMinutos + signo * totalminutos2;
+    
+        const nuevaHora = `${Math.floor(nuevoTotalMinutos / 60)}:${(nuevoTotalMinutos % 60).toString().padStart(2, '0')}`;
+        return nuevaHora;
+    }
   
         if (resultado.includes('-')) {
           const nuevaHoraResta = sumarRestarHoras(totalMinutosActual, resultado, true); 
