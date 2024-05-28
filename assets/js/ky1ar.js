001c5a507facde287c0d038cd301133a14ea406a
alex <alex@gmail.com>
2024-05-28 16:56:10 -0500

    edt

--- assets/js/ky1ar.js
+++ assets/js/ky1ar.js
@@ -255,7 +255,7 @@ $(document).ready(function () {
           resultado =
             resultadoHoras.toString().padStart(2, "0") + ":" + resultadoMinutos;
         }
-        console.log("data: " + resultado);
+        console.log("Semana " + semana + ": " + resultado);
         function sumarRestarHoras(
           totalMinutosActual,
           resultado,
