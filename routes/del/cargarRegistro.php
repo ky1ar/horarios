891373b3b7ea96a95b3cb579d39988afd7e76ede
alex <alex@gmail.com>
2024-05-28 15:31:33 -0500

    edt

--- routes/del/cargarRegistro.php
+++ routes/del/cargarRegistro.php
@@ -1,22 +1,36 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Verificar si se ha seleccionado un archivo
+    if (empty($_FILES['fileInput']['name'])) {
+        echo "Por favor, selecciona un archivo.";
+        exit; // Detener la ejecución del script si no se selecciona ningún archivo
+    }
+
     $targetDir = __DIR__ . '/../../';
     $targetFile = $targetDir . 'final.csv';
     $uploadOk = 1;
 
+    // Comprobar si el archivo es un CSV
     $fileType = pathinfo($_FILES['fileInput']['name'], PATHINFO_EXTENSION);
     if ($fileType !== 'csv') {
         echo "Solo se permiten archivos CSV.";
         $uploadOk = 0;
     }
+
+    // Verificar si hay errores al subir el archivo
     if ($_FILES['fileInput']['error'] !== UPLOAD_ERR_OK) {
         echo "Error al subir el archivo.";
         $uploadOk = 0;
     }
+
+    // Eliminar el archivo existente (si existe)
     if (file_exists($targetFile)) {
         unlink($targetFile);
     }
+
+    // Mover el archivo cargado a la ubicación de destino
     if ($uploadOk) {
+        // Establecer los permisos de escritura adecuados
         chmod($targetDir, 0777);
 
         if (move_uploaded_file($_FILES['fileInput']['tmp_name'], $targetFile)) {
@@ -26,5 +40,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 }
+
+// Redirigir de vuelta a la página principal después de cargar
 header('Location: ../../load.php');
 exit;
+?>
