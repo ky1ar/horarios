b2cff6f2c4f728cba35cf2eb384da7810c80e090
alex <alex@gmail.com>
2024-05-28 12:10:05 -0500

    edt

--- routes/del/cargarRegistro.php
+++ routes/del/cargarRegistro.php
@@ -28,6 +28,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 // Redirigir de vuelta a la página principal después de cargar
-header('Location: index.php');
+header('Location: load.php');
 exit;
 ?>
