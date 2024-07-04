6d8693bdccc794072d878a4ffbcf2bd1e770e929
alex <alex@gmail.com>
2024-07-04 15:03:12 -0500

    test

--- routes/del/get_info_user.php
+++ routes/del/get_info_user.php
@@ -26,8 +26,8 @@ if (isset($_POST['userId']) && isset($_POST['month']) && isset($_POST['year'])) 
         $penultDayCurrMonth = date('Y-m-d', strtotime("$penultDayCurrMonth -1 day"));
     }
     // Imprime las fechas calculadas
-    echo "Ultimo día laborable del mes anterior: $lastDayPrevMonth<br>";
-    echo "Penúltimo día laborable del mes actual: $penultDayCurrMonth<br>";
+    echo "Ultimo dia laborable del mes anterior: $lastDayPrevMonth<br>";
+    echo "Penultimo día laborable del mes actual: $penultDayCurrMonth<br>";
 
     $query = "SELECT
         u.id_user AS id_user,
