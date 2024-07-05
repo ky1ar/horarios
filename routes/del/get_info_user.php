f0d44d5eea9f80e8e140637f4748d6e3ba497f04
alex <alex@gmail.com>
2024-07-05 09:27:49 -0500

    test

--- routes/del/get_info_user.php
+++ routes/del/get_info_user.php
@@ -11,158 +11,97 @@ if (isset($_POST['userId']) && isset($_POST['month']) && isset($_POST['year'])) 
     $year = $_POST['year'];
     $currentDate = date('Y-m-d');
 
-    // Obtener el último día laborable del mes anterior
-    $lastWorkingDayOfLastMonthQuery = "SELECT MAX(calendar_date) AS last_working_day
-        FROM Calendar
-        WHERE calendar_date < DATE(CONCAT(?, '-', ?, '-01'))
-            AND DAYOFWEEK(calendar_date) BETWEEN 2 AND 6
-            AND holiday = 0;";
-    $stmtLastDay = $conn->prepare($lastWorkingDayOfLastMonthQuery);
-    $stmtLastDay->bind_param("si", $year, $month);
-    $stmtLastDay->execute();
-    $resultLastDay = $stmtLastDay->get_result();
-    $lastWorkingDay = $resultLastDay->fetch_assoc()['last_working_day'];
-    $stmtLastDay->close();
-
-    if (!$lastWorkingDay) {
-        echo json_encode(['success' => false, 'message' => 'No se pudo obtener el último día laborable del mes anterior.']);
-        exit();
-    }
-
-    // Obtener el penúltimo día laborable del mes actual
-    $penultimateDayOfMonthQuery = "SELECT MAX(calendar_date) AS penultimate_working_day
-        FROM (
-            SELECT calendar_date
-            FROM Calendar
-            WHERE calendar_date <= LAST_DAY(DATE(CONCAT(?, '-', ?, '-01')))
-                AND DAYOFWEEK(calendar_date) BETWEEN 2 AND 6
-                AND holiday = 0
-            ORDER BY calendar_date DESC
-            LIMIT 2
-        ) AS last_two_days
-        ORDER BY calendar_date ASC
-        LIMIT 1;";
-    $stmtPenultimateDay = $conn->prepare($penultimateDayOfMonthQuery);
-    $stmtPenultimateDay->bind_param("si", $year, $month);
-    $stmtPenultimateDay->execute();
-    $resultPenultimateDay = $stmtPenultimateDay->get_result();
-    $penultimateDayOfMonth = $resultPenultimateDay->fetch_assoc()['penultimate_working_day'];
-    $stmtPenultimateDay->close();
-
-    if (!$penultimateDayOfMonth) {
-        echo json_encode(['success' => false, 'message' => 'No se pudo obtener el penúltimo día laborable del mes actual.']);
-        exit();
-    }
-    // Consulta para calcular las horas ajustadas y el porcentaje de horas totales
     $query = "SELECT
-        u.id_user AS id_user,
-        u.id_profile,
-        SUM(
-            CASE
-                WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
-                WHEN u.id_profile = 2 THEN
-                    CASE
-                        WHEN DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
-                        WHEN DAYOFWEEK(c.calendar_date) = 7 THEN 4
-                        ELSE 0
-                    END
-                WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 THEN 8
-                ELSE 0
-            END
-        ) AS total_hours_required,
-        SUM(
-            ROUND(
+    u.id_user AS id_user,
+    u.id_profile,
+    SUM(
+        CASE
+            WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
+            WHEN u.id_profile = 2 THEN
                 CASE
-                    WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 AND c.calendar_date < DATE_SUB((SELECT MAX(stamp_date) FROM Archivos), INTERVAL 1 DAY) THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
-                    WHEN u.id_profile = 2 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 AND c.calendar_date < DATE_SUB((SELECT MAX(stamp_date) FROM Archivos), INTERVAL 1 DAY) THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
-                    WHEN u.id_profile = 2 AND DAYOFWEEK(c.calendar_date) = 7 AND c.calendar_date < DATE_SUB((SELECT MAX(stamp_date) FROM Archivos), INTERVAL 1 DAY) THEN GREATEST(0, (10 - COALESCE(LENGTH(s.stamp), 0)) / 5)
-                    WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 AND c.calendar_date < DATE_SUB((SELECT MAX(stamp_date) FROM Archivos), INTERVAL 1 DAY) THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                    WHEN DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
+                    WHEN DAYOFWEEK(c.calendar_date) = 7 THEN 4
                     ELSE 0
-                END, 0)
+                END
+            WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 THEN 8
+            ELSE 0
+        END
+    ) AS total_hours_required,
+       SUM(
+        ROUND(
+            CASE
+                WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 AND c.calendar_date < DATE_SUB((SELECT MAX(stamp_date) FROM Archivos), INTERVAL 1 DAY) THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                WHEN u.id_profile = 2 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 AND c.calendar_date < DATE_SUB((SELECT MAX(stamp_date) FROM Archivos), INTERVAL 1 DAY) THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                WHEN u.id_profile = 2 AND DAYOFWEEK(c.calendar_date) = 7 AND c.calendar_date < DATE_SUB((SELECT MAX(stamp_date) FROM Archivos), INTERVAL 1 DAY) THEN GREATEST(0, (10 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 AND c.calendar_date < DATE_SUB((SELECT MAX(stamp_date) FROM Archivos), INTERVAL 1 DAY) THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                ELSE 0
+            END, 0)
         ) + COALESCE(
             SUM(
                 CASE
                     WHEN c.calendar_date BETWEEN DATE(CONCAT(?, '-', ?, '-01')) AND DATE_SUB((SELECT MAX(stamp_date) FROM Archivos), INTERVAL 1 DAY)
                     THEN s.calc_diff ELSE 0 END), 0
         ) AS total_missing_points,
-        SUM(
-            CASE
-                WHEN LEFT(s.stamp, 5) > (CASE WHEN u.id_user = 13 THEN '10:00' ELSE '09:00' END) AND c.calendar_date < CURDATE() THEN 1
-                ELSE 0
-            END
-        ) AS total_late_points,
+    SUM(
+        CASE
+            WHEN LEFT(s.stamp, 5) > (CASE WHEN u.id_user = 13 THEN '10:00' ELSE '09:00' END) AND c.calendar_date < CURDATE() THEN 1
+            ELSE 0
+        END
+    ) AS total_late_points,
+    TIME_FORMAT(
+        SEC_TO_TIME(
+            SUM(
+                CASE
+                    WHEN LEFT(s.stamp, 5) > (CASE WHEN u.id_user = 13 THEN '10:00' ELSE '09:00' END) THEN 
+                        TIME_TO_SEC(LEFT(s.stamp, 5)) - TIME_TO_SEC(CASE WHEN u.id_user = 13 THEN '10:00' ELSE '09:00' END)
+                    ELSE 0
+                END
+            )
+        ), '%H:%i'
+    ) AS total_minutes_late_formatted,
+    CASE
+    WHEN SUM(
+            ROUND(
+                CASE
+                    WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 AND c.calendar_date < CURDATE() THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                    WHEN u.id_profile = 2 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 AND c.calendar_date < CURDATE() THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                    WHEN u.id_profile = 2 AND DAYOFWEEK(c.calendar_date) = 7 AND c.calendar_date < CURDATE() THEN GREATEST(0, (10 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                    WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 AND c.calendar_date < CURDATE() THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                    ELSE 0
+                END, 0)
+        ) > 6 THEN 
         TIME_FORMAT(
             SEC_TO_TIME(
-                SUM(
-                    CASE
-                        WHEN LEFT(s.stamp, 5) > (CASE WHEN u.id_user = 13 THEN '10:00' ELSE '09:00' END) THEN 
-                            TIME_TO_SEC(LEFT(s.stamp, 5)) - TIME_TO_SEC(CASE WHEN u.id_user = 13 THEN '10:00' ELSE '09:00' END)
-                        ELSE 0
-                    END
-                )
-            ), '%H:%i'
-        ) AS total_minutes_late_formatted,
-        CASE
-        WHEN SUM(
-                ROUND(
-                    CASE
-                        WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 AND c.calendar_date < CURDATE() THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
-                        WHEN u.id_profile = 2 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 AND c.calendar_date < CURDATE() THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
-                        WHEN u.id_profile = 2 AND DAYOFWEEK(c.calendar_date) = 7 AND c.calendar_date < CURDATE() THEN GREATEST(0, (10 - COALESCE(LENGTH(s.stamp), 0)) / 5)
-                        WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 AND c.calendar_date < CURDATE() THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
-                        ELSE 0
-                    END, 0)
-            ) > 6 THEN 
-            TIME_FORMAT(
-                SEC_TO_TIME(
-                    TIME_TO_SEC(
-                        SEC_TO_TIME(
-                            SUM(
-                                CASE
-                                    WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
-                                    WHEN u.id_profile = 2 THEN
-                                        CASE
-                                            WHEN DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
-                                            WHEN DAYOFWEEK(c.calendar_date) = 7 THEN 4
-                                            ELSE 0
-                                        END
-                                    WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 THEN 8
-                                    ELSE 0
-                                END
-                            ) * 60 * 60
-                        )
-                    ) + 
-                    (SUM(
-                        ROUND(
+                TIME_TO_SEC(
+                    SEC_TO_TIME(
+                        SUM(
                             CASE
-                                WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 AND c.calendar_date < CURDATE() THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
-                                WHEN u.id_profile = 2 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 AND c.calendar_date < CURDATE() THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
-                                WHEN u.id_profile = 2 AND DAYOFWEEK(c.calendar_date) = 7 AND c.calendar_date < CURDATE() THEN GREATEST(0, (10 - COALESCE(LENGTH(s.stamp), 0)) / 5)
-                                WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 AND c.calendar_date < CURDATE() THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                                WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
+                                WHEN u.id_profile = 2 THEN
+                                    CASE
+                                        WHEN DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
+                                        WHEN DAYOFWEEK(c.calendar_date) = 7 THEN 4
+                                        ELSE 0
+                                    END
+                                WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 THEN 8
                                 ELSE 0
-                            END, 0)
-                    ) - 6) * 15 * 60
-                ), '%H:%i'
-            )
-        ELSE
-            TIME_FORMAT(
-                SEC_TO_TIME(
-                    SUM(
+                            END
+                        ) * 60 * 60
+                    )
+                ) + 
+                (SUM(
+                    ROUND(
                         CASE
-                            WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
-                            WHEN u.id_profile = 2 THEN
-                                CASE
-                                    WHEN DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
-                                    WHEN DAYOFWEEK(c.calendar_date) = 7 THEN 4
-                                    ELSE 0
-                                END
-                            WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 THEN 8
+                            WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 AND c.calendar_date < CURDATE() THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                            WHEN u.id_profile = 2 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 AND c.calendar_date < CURDATE() THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                            WHEN u.id_profile = 2 AND DAYOFWEEK(c.calendar_date) = 7 AND c.calendar_date < CURDATE() THEN GREATEST(0, (10 - COALESCE(LENGTH(s.stamp), 0)) / 5)
+                            WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 AND c.calendar_date < CURDATE() THEN GREATEST(0, (20 - COALESCE(LENGTH(s.stamp), 0)) / 5)
                             ELSE 0
-                        END
-                    ) * 60 * 60
-                ), '%H:%i'
-            )
-        END AS adjusted_hours,
+                        END, 0)
+                ) - 6) * 15 * 60
+            ), '%H:%i'
+        )
+    ELSE
         TIME_FORMAT(
             SEC_TO_TIME(
                 SUM(
@@ -177,24 +116,42 @@ if (isset($_POST['userId']) && isset($_POST['month']) && isset($_POST['year'])) 
                         WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 THEN 8
                         ELSE 0
                     END
-                ) * 60 * 60 * 0.01
+                ) * 60 * 60
             ), '%H:%i'
-        ) AS one_percent_total_hours
-    FROM
-        Calendar c
-    JOIN
-        Users u ON u.id_user = ?
-    LEFT JOIN
-        Schedule s ON c.id_date = s.id_calendar AND s.id_user = ?
-    WHERE
-        c.calendar_date BETWEEN ? AND ?
-        AND c.holiday = 0
-    GROUP BY
-        u.id_user,
-        u.id_profile;";
+        )
+    END AS adjusted_hours,
+    TIME_FORMAT(
+        SEC_TO_TIME(
+            SUM(
+                CASE
+                    WHEN u.id_profile = 1 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
+                    WHEN u.id_profile = 2 THEN
+                        CASE
+                            WHEN DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 6 THEN 8
+                            WHEN DAYOFWEEK(c.calendar_date) = 7 THEN 4
+                            ELSE 0
+                        END
+                    WHEN u.id_profile = 3 AND DAYOFWEEK(c.calendar_date) BETWEEN 2 AND 7 THEN 8
+                    ELSE 0
+                END
+            ) * 60 * 60 * 0.01
+        ), '%H:%i'
+    ) AS one_percent_total_hours
+FROM
+    Calendar c
+JOIN
+    Users u ON u.id_user = ?
+LEFT JOIN
+    Schedule s ON c.id_date = s.id_calendar AND s.id_user = ?
+WHERE
+    c.calendar_date BETWEEN DATE(CONCAT(?, '-', ?, '-01')) AND LAST_DAY(DATE(CONCAT(?, '-', ?, '-01')))
+    AND c.holiday = 0
+GROUP BY
+    u.id_user,
+    u.id_profile;";
 
     $stmt = $conn->prepare($query);
-    $stmt->bind_param("ssiiss", $year, $month, $userId, $userId, $lastWorkingDay, $penultimateDayOfMonth);
+    $stmt->bind_param("ssiissss", $year, $month, $userId, $userId, $year, $month, $year, $month);
     $stmt->execute();
     $result = $stmt->get_result();
     $row = $result->fetch_assoc();
@@ -203,4 +160,3 @@ if (isset($_POST['userId']) && isset($_POST['month']) && isset($_POST['year'])) 
 } else {
     echo json_encode(['success' => false, 'message' => 'Invalid parameters.']);
 }
-?>
