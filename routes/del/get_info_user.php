debf25356d261b2b099ecb9621634c73b2230cb7
alex <alex@gmail.com>
2024-07-05 10:28:31 -0500

    test

--- routes/del/get_info_user.php
+++ routes/del/get_info_user.php
@@ -12,12 +12,12 @@ if (isset($_POST['userId']) && isset($_POST['month']) && isset($_POST['year'])) 
     $currentDate = date('Y-m-d');
 
     // Obtener el penúltimo día laborable del mes anterior
-        $penultimateQueryMonthPast = "
+    $penultimateQueryMonthPast = "
         SELECT calendar_date AS last_working_day_previous_month
         FROM Calendar
         WHERE holiday = 0
         AND DAYOFWEEK(calendar_date) <> 1
-        AND calendar_date BETWEEN DATE_SUB(DATE(CONCAT('?', '-', '?', '-01')), INTERVAL 1 MONTH) AND LAST_DAY(DATE_SUB(DATE(CONCAT('?', '-', '?', '-01')), INTERVAL 1 MONTH))
+        AND calendar_date BETWEEN DATE_SUB(DATE(CONCAT(?, '-', ?, '-01')), INTERVAL 1 MONTH) AND LAST_DAY(DATE_SUB(DATE(CONCAT(?, '-', ?, '-01')), INTERVAL 1 MONTH))
         ORDER BY calendar_date DESC
         LIMIT 1;
     ";
@@ -192,7 +192,7 @@ GROUP BY
     u.id_profile;";
 
     $stmt = $conn->prepare($query);
-    $stmt->bind_param("ssiiss", $year, $month, $userId, $userId,$penultimateMP, $penultimateWorkday);
+    $stmt->bind_param("ssiiss", $year, $month, $userId, $userId, $penultimateMP, $penultimateWorkday);
     $stmt->execute();
     $result = $stmt->get_result();
     $row = $result->fetch_assoc();
@@ -201,4 +201,3 @@ GROUP BY
 } else {
     echo json_encode(['success' => false, 'message' => 'Invalid parameters.']);
 }
-?>
