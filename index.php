9e62e574cb5f984d26bae6a003729d2b17dfc53b
alex <alex@gmail.com>
2024-07-20 13:37:19 -0500

    test

--- index.php
+++ index.php
@@ -15,33 +15,6 @@ require_once 'db.php';
 </head>
 
 <body>
-    <!-- <section id="login">
-        <div class="modal">
-            <div class="left">
-                <img src="assets/img/logod.webp" alt="Perfil">
-                <div>
-                    <h1>Bienvenido</h1>
-                    <p>Ingresa tus datos para iniciar sesión.</p>
-                </div>
-                <form id="loginForm">
-                    <div>
-                        <label for="document">DNI</label>
-                        <input name="document" type="text" placeholder="Ingresa tu DNI">
-                    </div>
-                    <div>
-                        <label for="pass">Contraseña</label>
-                        <input name="pass" type="password" placeholder="Ingresa tu contraseña">
-                    </div>
-                    <div id="errorDiv"></div>
-                    <button type="submit">Iniciar sesión</button>
-                </form>
-            </div>
-            <div class="right">
-                <img src="assets/img/login.webp" alt="">
-            </div>
-        </div>
-    </section> -->
-
     <section class="cont-login">
         <div class="login">
             <div>
