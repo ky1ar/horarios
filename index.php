f28a4bc876ee1577e2c98d77589f2e415d029237
alex <alex@gmail.com>
2024-07-20 12:49:28 -0500

    test

--- index.php
+++ index.php
@@ -45,8 +45,8 @@ require_once 'db.php';
     <section class="cont-login">
         <div class="login">
             <div>
-                <img src="./assets/img/nlog5.svg" alt="">
-                <img src="./assets/img/login-1.jpg" alt="">
+                <img class="logo" src="./assets/img/nlog5.svg" alt="">
+                <img class="refer" src="./assets/img/login-1.jpg" alt="">
             </div>
             <div>
                 <img src="./assets/img/user-icon.png" alt="">
