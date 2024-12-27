20a3102daa3fa07b22db15915c181a0f6b9e16a5
alex <alex@gmail.com>
2024-12-27 15:47:34 -0500

    a1

--- routes/del/getComments.php
+++ routes/del/getComments.php
@@ -1,10 +1,26 @@
 <?php
 require_once '../../includes/app/db.php';
 
+// Lista de los meses en español
+$mesesEnEspanol = [
+    'January' => 'Enero',
+    'February' => 'Febrero',
+    'March' => 'Marzo',
+    'April' => 'Abril',
+    'May' => 'Mayo',
+    'June' => 'Junio',
+    'July' => 'Julio',
+    'August' => 'Agosto',
+    'September' => 'Septiembre',
+    'October' => 'Octubre',
+    'November' => 'Noviembre',
+    'December' => 'Diciembre'
+];
+
 if (isset($_POST['id_user'])) {
     $id_user = (int)$_POST['id_user'];
 
-    $query = "SELECT c.comentario, DATE_FORMAT(c.created_at, '%d de %M del %Y') AS formatted_date
+    $query = "SELECT c.comentario, c.created_at
               FROM Comentarios c
               WHERE c.id_user = ?
               ORDER BY c.created_at DESC";
@@ -18,9 +34,23 @@ if (isset($_POST['id_user'])) {
     $comments = [];
     if ($result && mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
+            // Convertir la fecha a formato 'día de mes del año'
+            $date = $row['created_at'];
+            $timestamp = strtotime($date);
+            $day = date('d', $timestamp);
+            $month = date('F', $timestamp); // Mes en inglés
+            $year = date('Y', $timestamp);
+            
+            // Convertir el mes al español
+            if (array_key_exists($month, $mesesEnEspanol)) {
+                $month = $mesesEnEspanol[$month];
+            }
+
+            $formatted_date = "$day de $month del $year";
+
             $comments[] = [
                 'comentario' => htmlspecialchars($row['comentario']),
-                'created_at' => $row['formatted_date']
+                'created_at' => $formatted_date
             ];
         }
         echo json_encode(['success' => true, 'comments' => $comments]);
