aaa3b3e0fa2d4c23d13b4d19d642fd3b7dc89d90
alex <alex@gmail.com>
2024-12-27 10:59:16 -0500

    a1

--- routes/del/insertCommentBoss.php
+++ routes/del/insertCommentBoss.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../includes/app/db.php';
+require_once '../../includes/app/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['user_id']) && isset($_POST['comentario'])) {
