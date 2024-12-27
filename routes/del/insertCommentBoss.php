73a20b7c7a05de4be85825e2e67f74e5a2462cb2
alex <alex@gmail.com>
2024-12-27 10:57:08 -0500

    a1

--- routes/del/insertCommentBoss.php
+++ routes/del/insertCommentBoss.php
@@ -1,37 +1,28 @@
 <?php
-require_once '../../includes/app/db.php'; // Verifica la ruta de tu archivo db.php
+require_once '../includes/app/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Verificamos que los datos necesarios estén disponibles
     if (isset($_POST['user_id']) && isset($_POST['comentario'])) {
         $id_user = $_POST['user_id'];
-        $comentario = trim($_POST['comentario']);  // Limpiar el comentario
+        $comentario = trim($_POST['comentario']);
 
         if (!empty($comentario)) {
-            // Preparar la consulta SQL utilizando mysqli
             $sql = "INSERT INTO Comentarios (id_user, comentario) VALUES (?, ?)";
             $stmt = $conn->prepare($sql);
 
             if ($stmt) {
-                // Enlazar los parámetros y ejecutar la consulta
-                $stmt->bind_param("is", $id_user, $comentario); // "i" para enteros y "s" para string
+                $stmt->bind_param("is", $id_user, $comentario);
                 $stmt->execute();
-
-                // Mensaje de éxito
-                echo "Comentario agregado correctamente.";
                 $stmt->close();
-            } else {
-                echo "Error al preparar la consulta: " . $conn->error;
             }
-        } else {
-            // Si el comentario está vacío, mostramos un error
-            echo "El comentario no puede estar vacío.";
         }
+
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit();
     } else {
         echo "Faltan datos para agregar el comentario.";
     }
 }
 
-// Cerrar la conexión
 $conn->close();
 ?>
