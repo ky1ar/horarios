1bbf4953e9c7898d82254961809ff7032803ae64
alex <alex@gmail.com>
2024-12-27 11:12:43 -0500

    a1

--- routes/del/insertCommentBoss.php
+++ routes/del/insertCommentBoss.php
@@ -1,26 +1,22 @@
 <?php
-require_once '../../includes/app/db.php'; // Verifica la ruta de tu archivo db.php
+require_once '../../includes/app/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Verificar que los datos necesarios estén disponibles
     if (isset($_POST['user_id']) && isset($_POST['comentario'])) {
         $id_user = $_POST['user_id'];
-        $comentario = trim($_POST['comentario']);  // Limpiar el comentario
+        $comentario = trim($_POST['comentario']);
 
         if (!empty($comentario)) {
-            // Preparar la consulta SQL utilizando mysqli
             $sql = "INSERT INTO Comentarios (id_user, comentario) VALUES (?, ?)";
             $stmt = $conn->prepare($sql);
 
             if ($stmt) {
-                // Enlazar los parámetros y ejecutar la consulta
                 $stmt->bind_param("is", $id_user, $comentario);
                 $stmt->execute();
                 $stmt->close();
-
-                // Redirigir a la misma página para evitar reenvío del formulario
-                header("Location: " . $_SERVER['PHP_SELF']);
-                exit();
+                echo '<script type="text/javascript">
+                        window.location.reload();
+                      </script>';
             } else {
                 echo "Error al preparar la consulta: " . $conn->error;
             }
