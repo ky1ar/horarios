4a5566abf11b8d97e517dc2a5ab735f8e83cc4ad
alex <alex@gmail.com>
2024-12-28 11:52:39 -0500

    a1

--- load.php
+++ load.php
@@ -377,7 +377,7 @@ $id = $_SESSION['user_id'];
     </div>
 
     <div class="comentarios-boss" id="comments-container">
-        <h1>Comentarios</h1>
+        <h1>Notificaciones</h1>
         <div class="envio" style="display: <?php echo ($rango == 1) ? 'flex' : 'none'; ?>">
             <form id="commentForm">
                 <textarea id="commentb"></textarea>
