c679f7063f01b9a08eb8511c0095a7b65093162c
adriannc21 <alexandernajarro17@gmail.com>
2024-12-31 01:41:00 -0500

    px1

--- assets/js/ky1ar.js
+++ assets/js/ky1ar.js
@@ -291,7 +291,6 @@ $(document).ready(function () {
       success: function (response) {
         var calculatedTime = response.calculated_time;
         totalMonthlyTime = calculatedTime;
-        first = response.date
       },
       error: function (xhr, status, error) {
         console.error("Error en la solicitud AJAX:", error);
@@ -308,7 +307,6 @@ $(document).ready(function () {
       dataType: "json",
       success: function (response) {
         lastDayTime = response.calculated_time;
-        lastday = response.date
       },
       error: function (xhr, status, error) {
         console.error("Error en la solicitud AJAX:", error);
