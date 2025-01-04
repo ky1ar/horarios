641be94ab1bb46304e0f784e2c4e8f32e8cfea56
alex <alex@gmail.com>
2025-01-04 11:40:38 -0500

    a1

--- assets/js/ky1ar.js
+++ assets/js/ky1ar.js
@@ -282,6 +282,7 @@ $(document).ready(function () {
     var full_time = check2 ? 1 : 0;
     var salud = check3 ? 1 : 0;
     var servico = check4 ? 1 : 0;
+    console.log(servico);
     formData.append("mid_time", mid_time);
     formData.append("full_time", full_time);
     formData.append("salud", salud);
