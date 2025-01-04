ae967f77c77bc5a6a02dae22f90ff3a5ab39a4e3
alex <alex@gmail.com>
2025-01-04 10:23:13 -0500

    a1

--- load.php
+++ load.php
@@ -33,7 +33,7 @@ $id = $_SESSION['user_id'];
     <section id="ky1-rgt">
         <header>
             <div class="ky1-ttl">
-                <h1>Horarios</h1>
+                <h1>Horarios Krear 3D</h1>
                 <span>Registro biométrico del mes</span>
             </div>
             <div class="ky1-permisos" style="display: <?php echo ($rango == 1) ? 'none' : 'flex'; ?>">
