22c5464db723e168d8083bddcbdb5c0add4522ab
alex <alex@gmail.com>
2025-01-07 17:54:29 -0500

    a1

--- header.php
+++ header.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" href="assets/css/ky1ar.css?v=0.28">
+<link rel="stylesheet" href="assets/css/ky1ar.css?v=0.29">
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="assets/js/ky1ar.js?v=0.28"></script>
