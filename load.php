4b21744173a12126bbb199148a1742c8147d55e8
alex <alex@gmail.com>
2025-04-03 12:37:00 -0500

    Points Ventas

--- load.php
+++ load.php
@@ -439,7 +439,7 @@ $id = $_SESSION['user_id'];
 
     <div class="comentarios-boss" id="comments-container">
         <h1>Notificaciones</h1>
-        <div class="envio">
+        <div class="envio" style="display: <?php echo in_array($id, [11, 20, 25]) ? 'flex' : 'none'; ?>;">
             <form id="commentForm">
                 <textarea id="commentb"></textarea>
                 <input type="submit" value="Comentar">
