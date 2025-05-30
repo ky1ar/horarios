a2b85b7e985bcdc0928a32930a9e4e4101d24d7f
alex <alex@gmail.com>
2025-05-30 09:52:55 -0500

    Edt Horarios

--- load.php
+++ load.php
@@ -455,7 +455,7 @@ $stmt->close();
 
     <div class="comentarios-boss" id="comments-container">
         <h1>Notificaciones</h1>
-        <div class="envio" id="sec-mes-env" style="display: <?php echo in_array($id2, [11, 25, 20]) ? 'flex' : 'none'; ?>" data-user-id="<?php echo $id2; ?>" data-user-name="<?php echo $name; ?>">
+        <div class="envio" id="sec-mes-env" style="display: <?php echo in_array($id2, [2, 19, 11, 25, 20]) ? 'flex' : 'none'; ?>" data-user-id="<?php echo $id2; ?>" data-user-name="<?php echo $name; ?>">
             <form id="commentForm">
                 <textarea id="commentb"></textarea>
                 <input type="submit" value="Comentar">
