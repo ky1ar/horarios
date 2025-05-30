b148e91265b48914ae7d0d931528a4d508d19883
alex <alex@gmail.com>
2025-05-30 09:42:29 -0500

    Edt Horarios

--- load.php
+++ load.php
@@ -64,7 +64,7 @@ $stmt->close();
                 <img id="nextMonth" src="assets/img/r.svg" width="12" height="12" alt="">
             </div>
             <div id="indice">
-                <h1>Leyenda</h1>
+                <h1>Leyenda3</h1>
                 <p><span></span> Normal</p>
                 <p><span></span> Modificado</p>
                 <p><span></span> Permiso de Salud</p>
